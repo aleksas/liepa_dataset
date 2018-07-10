da13b0b090def1bda0d151c12706134d2d7fef12
Aleksas Pielikis <aleksas@neurotechnology.com>
2018-07-10 10:56:55 +0300

    Update script to download LIEPA 4 voices with -s flag.

--- liepa.py
+++ liepa.py
@@ -14,6 +14,10 @@ default_wav_subtype = 'PCM_16'
 default_archive_path = './MII_LIEPA_v1.tar.bz2'
 default_annotation_archive_path = './annotations.tar.bz2'
 default_dir = './MII_LIEPA_V1'
+default_aiste_archive_path = './Aiste.7z'
+default_regina_archive_path = './Regina.7z'
+default_edvardas_archive_path = './Edvardas.7z'
+default_vladas_archive_path = './Vladas.7z'
 
 filname_pattern = compile(r'(?P<type>ZS?|SS?)?(?P<voice>\d+)(?P<sex>M|V)(?P<age>[a-r])_(?P<ut_id>(?P<ut_id_d>\d+)[abc]?)(_(?P<ut_subid>\d+[abc]?))?(?P<tag>_[TP])?(?P<ext>\.(wav|txt))')
 
