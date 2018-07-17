63cc20cff458518dc1740d3ee609ee27417c05d9
Aleksas Pielikis <aleksas@neurotechnology.com>
2018-07-17 11:13:58 +0300

    Removed redundant voice specific extraction.

--- get_liepa.py
+++ get_liepa.py
@@ -1,10 +1,10 @@
 from os.path import exists
 from utils.download_google_drive_file import download_file_from_google_drive
-from utils.untar import extract_subfolders, extract_all
+from utils.untar import extract_all
 from argparse import ArgumentParser
 from os import rename
 
-from liepa import rec_dataset_voices, default_rec_dir, default_syn_dir, default_ann_dir
+from liepa import default_rec_dir, default_syn_dir, default_ann_dir
 
 # For manual download us link: https://drive.google.com/open?id=[ID]
 liepa_voice_recognition_dataset_file_id = '1GSzu9n7I-mUMfaD7jkq_CvwZlZXbz9c7'
@@ -42,17 +42,6 @@ regina_mp3_path = './Regina.mp3'
 edvardas_mp3_path = './Edvardas.mp3'
 vladas_mp3_path = './Vladas.mp3'
 
-# Very slow, better extract all
-def extract_specific_voices(archive_path, dst_dataset_directory, voices):
-    # Verify voice names
-    for voice in voices:
-        if voice not in all_voices:
-            raise Exception('"%s" is not a valid name.')
-
-    # Extract specific voices, very slow
-    subfolders = ['%s/' % voice for voice in voices]
-    extract_subfolders(archive_path, subfolders, dst_dataset_directory)
-
 if __name__ == '__main__':
 
     parser = ArgumentParser()
@@ -63,7 +52,6 @@ if __name__ == '__main__':
     parser.add_argument('-e','--liepa-rec-dir', help='Directory for LIEPA recognition dataset to unpack to. (default: "%s")' % default_rec_dir, default=default_rec_dir)
     parser.add_argument('-d','--liepa-syn-dir', help='Directory for LIEPA synth dataset to unpack to. (default: "%s")' % default_syn_dir, default=default_syn_dir)
     parser.add_argument('-f','--force', help='Overwrite files if present.',  action='store_true')
-    parser.add_argument('-v','--voices', nargs='+', help='List of voices to unpack (e.g. -v D256 D512).')
     args = parser.parse_args()
 
     local_liepa_rec_dataset_directory = args.liepa_rec_dir
@@ -78,7 +66,6 @@ if __name__ == '__main__':
             local_archive_tmp_path = local_archive_path + ".downloading"
             download_file_from_google_drive(google_drive_file_id, local_archive_tmp_path)
             rename(local_archive_tmp_path, local_archive_path)
-
 
     if args.get_annotations:
         dounload_file(default_annotation_archive_path, liepa_voice_recognition_dataset_annotation_file_id)
@@ -113,8 +100,6 @@ if __name__ == '__main__':
     if args.get_rec_voices:
         dounload_file(default_rec_archive_path, liepa_voice_recognition_dataset_file_id)
 
-        if not exists(local_liepa_rec_dataset_directory) or args.force:
-            if args.extract and args.voices:
-                extract_specific_voices(default_rec_archive_path, local_liepa_rec_dataset_directory, args.voices)
-            else:
-                extract_all(default_rec_archive_path, local_liepa_rec_dataset_directory)
+        if args.extract:
+            if not exists(local_liepa_rec_dataset_directory) or args.force:
+                    extract_all(default_rec_archive_path, local_liepa_rec_dataset_directory)
