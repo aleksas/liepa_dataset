73311bed7a6e9330d0e06d00e808e9a4558de47d
Aleksas Pielikis <ant.kampo@gmail.com>
2018-07-27 20:25:56 +0300

    Added m4a file downloading.

--- get_liepa.py
+++ get_liepa.py
@@ -23,8 +23,8 @@ default_edvardas_archive_path = './MII_LIEPA_SYN_Edvardas_v1.tar.bz2'
 default_vladas_archive_path = './MII_LIEPA_SYN_Vladas_v1.tar.bz2'
 
 liepa_voice_synth_aiste_wav_file_id = '1myOg7BKRrisbMXiS1u_LniElVSLThKc2'
-liepa_voice_synth_edvardas_wav_file_id = '1_Pw2DroFKmRWq8q59DfknCzikAFVEJfq'
 liepa_voice_synth_regina_wav_file_id = '16KviJdnB6vmIPxcMhun5u0puECzEEcfP'
+liepa_voice_synth_edvardas_wav_file_id = '1_Pw2DroFKmRWq8q59DfknCzikAFVEJfq'
 liepa_voice_synth_vladas_wav_file_id = '1AaWtUlx0cL0e2KeonN1zhKBsPmMOptuC'
 
 aiste_wav_path = './Aiste.wav'
@@ -33,14 +33,24 @@ edvardas_wav_path = './Edvardas.wav'
 vladas_wav_path = './Vladas.wav'
 
 liepa_voice_synth_aiste_mp3_file_id = '1OAHpTQqA6fRyq9EJeVrHF5Syxr1RO7_b'
-liepa_voice_synth_edvardas_mp3_file_id = '12bVW8n9JgT9OMdmWrqs4o7a9C32rPmdA'
 liepa_voice_synth_regina_mp3_file_id = '1o2pmQwSHJpR1ZA7VIfBGwJfHqN4SBEBv'
+liepa_voice_synth_edvardas_mp3_file_id = '12bVW8n9JgT9OMdmWrqs4o7a9C32rPmdA'
 liepa_voice_synth_vladas_mp3_file_id = '1VAyhUUgvOq-UXoOIowCUdu0AhRV-FnPy'
 
 aiste_mp3_path = './Aiste.mp3'
 regina_mp3_path = './Regina.mp3'
 edvardas_mp3_path = './Edvardas.mp3'
 vladas_mp3_path = './Vladas.mp3'
+
+liepa_voice_synth_aiste_m4a_file_id = '17bCNRPC4qCpNasFoyop3GLf8o8Ejc60Z'
+liepa_voice_synth_regina_m4a_file_id = '1UO9_7eGzis0-dTCWgB5dJccVAdWpD87F'
+liepa_voice_synth_edvardas_m4a_file_id = '1fEuXE9oLyrCGHMIVQhjh0HUyBaOcqAsh'
+liepa_voice_synth_vladas_m4a_file_id = '1E2kO0hipO7LRcBulCfaGvWJ_yv6LP7V5'
+
+aiste_m4a_path = './Aiste.m4a'
+regina_m4a_path = './Regina.m4a'
+edvardas_m4a_path = './Edvardas.m4a'
+vladas_m4a_path = './Vladas.m4a'
 
 if __name__ == '__main__':
 
@@ -89,6 +99,11 @@ if __name__ == '__main__':
         dounload_file(regina_mp3_path, liepa_voice_synth_regina_mp3_file_id)
         dounload_file(edvardas_mp3_path, liepa_voice_synth_edvardas_mp3_file_id)
         dounload_file(vladas_mp3_path, liepa_voice_synth_vladas_mp3_file_id)
+        
+        dounload_file(aiste_m4a_path, liepa_voice_synth_aiste_m4a_file_id)
+        dounload_file(regina_m4a_path, liepa_voice_synth_regina_m4a_file_id)
+        dounload_file(edvardas_m4a_path, liepa_voice_synth_edvardas_m4a_file_id)
+        dounload_file(vladas_m4a_path, liepa_voice_synth_vladas_m4a_file_id)
 
         if args.extract:
             if not exists(default_syn_dir) or args.force:
